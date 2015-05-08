215e26e5664c146f1d914bf9f98f0959b5cc3204
Miguel <miguel.angel.lopez@correounivalle.edu.co>
2015-05-08 17:49:17 -0500

    Interfaz terminada

--- src/Interfaz/MenuPrincipal.java
+++ src/Interfaz/MenuPrincipal.java
@@ -20,6 +20,7 @@ public class MenuPrincipal extends JFrame{
     JButton buttonSaveFiles;
     JButton buttonLoadFiles;
     JButton buttonStart;
+    JButton buttonGenerate;
     JTextField textFieldSaveFiles;
     JTextField textFieldLoadFiles;
     JRadioButton radioButtonComiteExaust;
@@ -30,6 +31,9 @@ public class MenuPrincipal extends JFrame{
     JRadioButton radioButtonDistUni;
     JRadioButton radioButtonDistNor;
     ButtonGroup buttonGroupDist;
+    JSpinner spinnerNumDistri;
+    JTextArea textAreaNaive;
+    JTextArea textAreaSmart;
 
     public MenuPrincipal(){
         super("Mini Proyecto");
@@ -38,6 +42,7 @@ public class MenuPrincipal extends JFrame{
         buttonSaveFiles = new JButton("Guardar");
         buttonLoadFiles = new JButton("Examinar");
         buttonStart = new JButton("Comenzar");
+        buttonGenerate = new JButton("Generar");
         textFieldSaveFiles = new JFormattedTextField(new File("").getAbsolutePath() + "/prueba.txt");
         textFieldSaveFiles.setEditable(false);
         textFieldLoadFiles = new JFormattedTextField(new File("").getAbsolutePath() + "/prueba.txt");
@@ -56,139 +61,177 @@ public class MenuPrincipal extends JFrame{
         buttonGroupDist = new ButtonGroup();
         buttonGroupDist.add(radioButtonDistUni);
         buttonGroupDist.add(radioButtonDistNor);
-
+        spinnerNumDistri = new JSpinner(new SpinnerNumberModel(5,1,50000,1));
+        textAreaNaive = new JTextArea();
+        textAreaSmart = new JTextArea();
 
         //Se crea el JPanel que contendra todos los demas componentes
         JPanel panelPrincipal = new JPanel(new GridBagLayout());
-
 
         //Panel contenedor del titulo
         JPanel panelTitle = new JPanel(new GridLayout(1,1));
         GridBagConstraints constraintsTitle = new GridBagConstraints();
         constraintsTitle.gridx = 1;
         constraintsTitle.gridy = 0;
-        constraintsTitle.gridwidth = 1;
+        constraintsTitle.gridwidth = 3;
         constraintsTitle.gridheight = 1;
-        constraintsTitle.weighty = 1;
+        constraintsTitle.insets = new Insets(20,20,20,20);
         JLabel labelTitle = new JLabel("Mini Proyecto", JLabel.CENTER);
         labelTitle.setFont(new Font("Arial", Font.BOLD, 60));
         labelTitle.setForeground(Color.decode("#aed581"));
         panelTitle.add(labelTitle);
+        panelPrincipal.add(panelTitle,constraintsTitle);
 
         //Panel contenedor del label Problemas
         JPanel panelLabelProblema = new JPanel(new GridLayout(1,1));
         GridBagConstraints constraintsLabelProblema = new GridBagConstraints();
         constraintsLabelProblema.gridx = 1;
         constraintsLabelProblema.gridy = 1;
-        constraintsLabelProblema.gridwidth = 1;
+        constraintsLabelProblema.gridwidth = 3;
         constraintsLabelProblema.gridheight = 1;
+        constraintsLabelProblema.insets = new Insets(10,10,10,10);
         JLabel labelTitleProblema = new JLabel("Problemas", JLabel.CENTER);
         labelTitleProblema.setFont(new Font("Arial", Font.BOLD, 20));
         panelLabelProblema.add(labelTitleProblema);
-
-        //Panel contenedor de radioButton y buttonStart
-//        JPanel panelOpciones = new JPanel(new GridLayout(2,1));
+        panelPrincipal.add(panelLabelProblema, constraintsLabelProblema);
 
         //Panel contenedor de radioButton
-        JPanel panelRadioButtonProb = new JPanel(new GridLayout(3, 2, 0, 0));
         GridBagConstraints constraintsRadButProb = new GridBagConstraints();
         constraintsRadButProb.gridx = 1;
         constraintsRadButProb.gridy = 2;
         constraintsRadButProb.gridwidth = 1;
         constraintsRadButProb.gridheight = 1;
-        panelRadioButtonProb.add(radioButtonComiteExaust);
-        panelRadioButtonProb.add(radioButtonProcExaust);
-        panelRadioButtonProb.add(radioButtonComiteSmart);
-        panelRadioButtonProb.add(radioButtonProcSmart);
+        constraintsRadButProb.anchor = GridBagConstraints.WEST;
+        panelPrincipal.add(radioButtonComiteExaust, constraintsRadButProb);
+        constraintsRadButProb.gridx = 2;
+        constraintsRadButProb.gridy = 2;
+        constraintsRadButProb.gridwidth = 2;
+        constraintsRadButProb.gridheight = 1;
+        panelPrincipal.add(radioButtonProcExaust, constraintsRadButProb);
+        constraintsRadButProb.gridx = 1;
+        constraintsRadButProb.gridy = 3;
+        constraintsRadButProb.gridwidth = 1;
+        constraintsRadButProb.gridheight = 1;
+        panelPrincipal.add(radioButtonComiteSmart, constraintsRadButProb);
+        constraintsRadButProb.gridx = 2;
+        constraintsRadButProb.gridy = 3;
+        constraintsRadButProb.gridwidth = 2;
+        constraintsRadButProb.gridheight = 1;
+        panelPrincipal.add(radioButtonProcSmart, constraintsRadButProb);
 
         //Panel contenedor del label Problemas
         JPanel panelLabelDistri = new JPanel(new GridLayout(1,1));
         GridBagConstraints constraintsLabelDistri = new GridBagConstraints();
         constraintsLabelDistri.gridx = 1;
-        constraintsLabelDistri.gridy = 3;
-        constraintsLabelDistri.gridwidth = 1;
+        constraintsLabelDistri.gridy = 4;
+        constraintsLabelDistri.gridwidth = 3;
         constraintsLabelDistri.gridheight = 1;
+        constraintsLabelDistri.insets = new Insets(10,10,10,10);
         JLabel labelTitleDistri = new JLabel("Distribuciones", JLabel.CENTER);
         labelTitleDistri.setFont(new Font("Arial", Font.BOLD, 17));
         panelLabelDistri.add(labelTitleDistri);
+        panelPrincipal.add(panelLabelDistri, constraintsLabelDistri);
 
         //Panel contenedor del radioButton de las distribuciones
-        JPanel panelRadioButtonDistri = new JPanel(new GridLayout(1, 2, 0, 0));
         GridBagConstraints constraintsRadButDistri = new GridBagConstraints();
         constraintsRadButDistri.gridx = 1;
-        constraintsRadButDistri.gridy = 4;
+        constraintsRadButDistri.gridy = 5;
         constraintsRadButDistri.gridwidth = 1;
         constraintsRadButDistri.gridheight = 1;
-        constraintsRadButDistri.weightx = 1.0;
-        panelRadioButtonDistri.add(radioButtonDistUni);
-        panelRadioButtonDistri.add(radioButtonDistNor);
+        panelPrincipal.add(radioButtonDistUni, constraintsRadButDistri);
+        constraintsRadButDistri.gridx = 2;
+        constraintsRadButDistri.gridy = 5;
+        constraintsRadButDistri.gridwidth = 1;
+        constraintsRadButDistri.gridheight = 1;
+        panelPrincipal.add(radioButtonDistNor, constraintsRadButDistri);
+        constraintsRadButDistri.gridx = 3;
+        constraintsRadButDistri.gridy = 5;
+        constraintsRadButDistri.gridwidth = 1;
+        constraintsRadButDistri.gridheight = 1;
+        constraintsRadButDistri.anchor = GridBagConstraints.WEST;
+        constraintsRadButDistri.fill = GridBagConstraints.HORIZONTAL;
+        constraintsRadButDistri.insets = new Insets(0,10,0,10);
+        panelPrincipal.add(spinnerNumDistri, constraintsRadButDistri);
 
         //Panel contenedor de guardar archivos
-        JPanel panelSaveFilesContent = new JPanel(new BorderLayout(20, 20));
         GridBagConstraints constraintsSaveFiles = new GridBagConstraints();
         constraintsSaveFiles.gridx = 1;
         constraintsSaveFiles.gridy = 6;
+        constraintsSaveFiles.gridwidth = 2;
+        constraintsSaveFiles.gridheight = 1;
+        constraintsSaveFiles.insets = new Insets(10,10,10,10);
+        constraintsSaveFiles.fill = GridBagConstraints.BOTH;
+        panelPrincipal.add(textFieldSaveFiles, constraintsSaveFiles);
+        constraintsSaveFiles.gridx = 3;
+        constraintsSaveFiles.gridy = 6;
         constraintsSaveFiles.gridwidth = 1;
         constraintsSaveFiles.gridheight = 1;
-        constraintsSaveFiles.weighty = 1;
-        constraintsSaveFiles.fill = GridBagConstraints.HORIZONTAL;
-//        constraintsSaveFiles.anchor = GridBagConstraints.NORTH;
-        panelSaveFilesContent.add(new JLabel("      "), BorderLayout.WEST);
-        panelSaveFilesContent.add(textFieldSaveFiles, BorderLayout.CENTER);
-        JPanel panelButtonSaveFiles = new JPanel(new BorderLayout());
-        panelButtonSaveFiles.add(buttonSaveFiles, BorderLayout.CENTER);
-        panelButtonSaveFiles.add(new JLabel("    "), BorderLayout.EAST);
-        panelSaveFilesContent.add(panelButtonSaveFiles, BorderLayout.EAST);
+        panelPrincipal.add(buttonSaveFiles, constraintsSaveFiles);
 
         //Panel contenedor de guardar archivos
-        JPanel panelLoadFilesContent = new JPanel(new BorderLayout(20, 20));
         GridBagConstraints constraintsLoadFiles = new GridBagConstraints();
         constraintsLoadFiles.gridx = 1;
         constraintsLoadFiles.gridy = 7;
+        constraintsLoadFiles.gridwidth = 2;
+        constraintsLoadFiles.gridheight = 1;
+        constraintsLoadFiles.insets = new Insets(10,10,10,10);
+        constraintsLoadFiles.fill = GridBagConstraints.BOTH;
+        panelPrincipal.add(textFieldLoadFiles, constraintsLoadFiles);
+        constraintsLoadFiles.gridx = 3;
+        constraintsLoadFiles.gridy = 7;
         constraintsLoadFiles.gridwidth = 1;
         constraintsLoadFiles.gridheight = 1;
-        constraintsLoadFiles.weighty = 1;
-        constraintsLoadFiles.fill = GridBagConstraints.HORIZONTAL;
-        constraintsLoadFiles.anchor = GridBagConstraints.NORTH;
-        panelLoadFilesContent.add(new JLabel("      "), BorderLayout.WEST);
-        panelLoadFilesContent.add(textFieldLoadFiles, BorderLayout.CENTER);
-        JPanel panelButtonLoadFiles = new JPanel(new BorderLayout());
-        panelButtonLoadFiles.add(buttonLoadFiles, BorderLayout.CENTER);
-        panelButtonLoadFiles.add(new JLabel("    "), BorderLayout.EAST);
-        panelLoadFilesContent.add(panelButtonLoadFiles, BorderLayout.EAST);
+        panelPrincipal.add(buttonLoadFiles, constraintsLoadFiles);
 
         //Panel contenedor de buttonStart
-        JPanel panelButtonStart = new JPanel(new FlowLayout());
         GridBagConstraints constraintsButStart = new GridBagConstraints();
         constraintsButStart.gridx = 1;
         constraintsButStart.gridy = 8;
         constraintsButStart.gridwidth = 1;
         constraintsButStart.gridheight = 1;
-        panelButtonStart.add(buttonStart);
+        constraintsButStart.insets = new Insets(10,10,10,10);
+        panelPrincipal.add(buttonStart, constraintsButStart);
+        constraintsButStart.gridx = 2;
+        constraintsButStart.gridy = 8;
+        constraintsButStart.gridwidth = 1;
+        constraintsButStart.gridheight = 1;
+        panelPrincipal.add(buttonGenerate, constraintsButStart);
 
-        //Añade los radioButton y el buttonStart a el contenedor
-//        panelOpciones.add(new JLabel("           "),BorderLayout.WEST);
-//        panelOpciones.add(panelRadioButton);
-//        panelOpciones.add(panelStart);
+        GridBagConstraints constraintsLabelTexDistri = new GridBagConstraints();
+        constraintsLabelTexDistri.gridx = 1;
+        constraintsLabelTexDistri.gridy = 9;
+        constraintsLabelTexDistri.gridwidth = 1;
+        constraintsLabelTexDistri.gridheight = 1;
+        constraintsLabelTexDistri.insets = new Insets(10,10,10,10);
+        panelPrincipal.add(new JLabel("Algoritmo Ingenuo"), constraintsLabelTexDistri);
+        constraintsLabelTexDistri.gridx = 2;
+        constraintsLabelTexDistri.gridy = 9;
+        constraintsLabelTexDistri.gridwidth = 1;
+        constraintsLabelTexDistri.gridheight = 1;
+        constraintsLabelTexDistri.insets = new Insets(10,10,10,10);
+        panelPrincipal.add(new JLabel("Algoritmo Inteligente"), constraintsLabelTexDistri);
 
-        //Añade el buscador de archivos y lao opciones de busqueda al contenedor del centro
-//        panelContenidoMedio.add(panelFilesContent, BorderLayout.NORTH);
+        GridBagConstraints constraintsTextAlgorithms = new GridBagConstraints();
+        constraintsTextAlgorithms.gridx = 1;
+        constraintsTextAlgorithms.gridy = 10;
+        constraintsTextAlgorithms.gridwidth = 1;
+        constraintsTextAlgorithms.gridheight = 1;
+        constraintsTextAlgorithms.insets = new Insets(10,10,10,10);
+        constraintsTextAlgorithms.fill = GridBagConstraints.BOTH;
+        constraintsTextAlgorithms.weighty = 1;
+        panelPrincipal.add(new JScrollPane(textAreaNaive), constraintsTextAlgorithms);
+        constraintsTextAlgorithms.gridx = 2;
+        constraintsTextAlgorithms.gridy = 10;
+        constraintsTextAlgorithms.gridwidth = 1;
+        constraintsTextAlgorithms.gridheight = 1;
+        constraintsTextAlgorithms.insets = new Insets(10,10,10,10);
+        panelPrincipal.add(new JScrollPane(textAreaSmart), constraintsTextAlgorithms);
 
-
-        //Añade el titulo y las opciones del centro a la ventana principal
-        panelPrincipal.add(panelTitle,constraintsTitle);
-        panelPrincipal.add(panelLabelProblema, constraintsLabelProblema);
-        panelPrincipal.add(panelRadioButtonProb, constraintsRadButProb);
-        panelPrincipal.add(panelLabelDistri, constraintsLabelDistri);
-        panelPrincipal.add(panelRadioButtonDistri, constraintsRadButDistri);
-        panelPrincipal.add(panelSaveFilesContent, constraintsSaveFiles);
-        panelPrincipal.add(panelLoadFilesContent, constraintsLoadFiles);
-        panelPrincipal.add(panelButtonStart, constraintsButStart);
 
         add(panelPrincipal);
 
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(600, 400);
+        setSize(800, 700);
         setVisible(true);
     }
 }
